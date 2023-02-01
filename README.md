8d4f3e89531f57d22e29542335454ea3199082d5
Carter <carter.hemington@gmail.com>
2023-02-01 09:34:17 -0500

    functionality complete

--- README.md
+++ README.md
@@ -7,4 +7,3 @@
 * check that max width and total space is used - 960px
 * boxes adapt to space size
 * If prompt cancel, have to make sure that grid stays at default 16 (maybe just include it as calling the function with the default in the if statement)
-* Add button that when pressed removes highlight class from all boxes
--- functions.js
+++ functions.js
@@ -7,8 +7,6 @@ container.addEventListener('mouseover', function (event) {
   //attaching event listener to parent for ease of use
   if (event.target.matches('.box')) {
     //if the item in question is box
-    console.log("mouseover event")
-    // todo remove this console log
     event.target.classList.add('highlight');
     //add highlight class to it
   }
@@ -22,21 +20,25 @@ document.querySelector(".createGrid").onclick = function() {
   let promptAnswer =  prompt("How many columns and rows would you like?");
   //created a prompt to receive a value
   if(promptAnswer < 100) {
+    //limit of 100 added due to Project details
     makeBoxes(promptAnswer);
   } else if (promptAnswer >= 100) {
+    //if above limit, let the user know with an alert
     alert("Input number must be less than 100!")
   }
 }
 
 document.querySelector(".removeHighlight").onclick = function() {
   //captured the button and prompted to run anonymous function when clicked
-  console.log("added again")
-  //todo remove this console.log
   const highlightedBoxes = document.querySelectorAll(".box.highlight");
+  //creating selector for items that have been highlighted
   
   highlightedBoxes.forEach((item) => {
+    //looping through each item selected
       if (item !== null) {
+        //if the item exists
         item.classList.remove("highlight")
+        //remove the highlighted class from it
       }
     } 
   )
@@ -45,7 +47,7 @@ document.querySelector(".removeHighlight").onclick = function() {
 function makeBoxes(numBoxes) {
   //function to draw the boxes
   let boxNumber = 1;
-  //creating varialbe to add a number to each box
+  //creating variable to add a number to each box
   container.replaceChildren();
   //before we start the loop - remove any existing boxes
 
@@ -57,11 +59,11 @@ function makeBoxes(numBoxes) {
       const square = document.createElement("div");
       // create a div
       square.classList.add("box");
-      // add class "box" to id
+      // add class "box" to it
       square.textContent = boxNumber;
       // add text in the box of the current number
       container.append(square);
-      // append that box to the container
+      // append that div to the container
       boxNumber += 1;
       //increase the number once actions have been completed
   }
